58b56df75174f778104a64b2c86e136f1fe9d3e7
YuliiaHarkusha <Yuliia.Harkusha27@gmail.com>
2025-08-12 09:23:33 +0200

    update index.html, index.js

--- src/js/Index.js
+++ src/js/Index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./Components/App";
+import "../scss/layout/_main.scss";
 
 const container = document.getElementById("app");
 const root = createRoot(container);
